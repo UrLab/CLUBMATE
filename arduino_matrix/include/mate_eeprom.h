fd8c3361ded025e1844693c416202df9cf39c0ad
Thibault Vanwersch <thibault@vanwersch.fr>
2019-02-09 14:58:36 +0100

    (WIP) some code for eeprom handling (not-tested lul)

---
+++ arduino_matrix/include/mate_eeprom.h
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include "utils.h"
+
+#define CRATE_WIDTH_ADDR 0
+#define CRATE_HEIGHT_ADDR 1
+#define CRATE_SIZE_ADDR 2
+
+#define MATRIX_WIDTH_ADDR 3
+#define MATRIX_HEIGHT_ADDR 4
+#define MATRIX_SIZE_ADDR 5
+#define MATRIX_START_ADDR 6
+#define DYN_MEMORY_ADDR 7
+
+/* Information about a crate.
+ */
+uint8_t get_crate_width();
+void set_crate_width(uint8_t w);
+
+uint8_t get_crate_height();
+void set_crate_height(uint8_t h);
+
+coord* get_crate_shape();
+
+/* Information about the matrix (in number of crate).
+ */
+uint8_t get_matrix_width();
+void set_matrix_width(uint8_t w);
+
+uint8_t get_matrix_height();
+void set_matrix_height(uint8_t h);
+
+coord* get_crate_shape();
+void set_crate_shape(coord* shape, size_t size);
+
+coord* get_matrix_shape();
+
+/* Set shapes of the matrix.
+ */
+void set_shape(coord* crate_shape,  size_t crate_size,
+               coord* matrix_shape, size_t matrix_size);
---
+++ arduino_matrix/src/mate_eeprom.cpp
@@ -0,0 +1,82 @@
+#include <EEPROM.h>
+
+#include "mate_eeprom.h"
+
+coord* _get_range(uint8_t from, uint8_t size) {
+    coord* range = (coord *) malloc(sizeof(coord) * size);
+
+    for (uint8_t ptr = from; ptr <= (ptr + (2 * size)); ptr += 2) {
+        range->x = (uint8_t) EEPROM.read(ptr);
+        range->y = (uint8_t) EEPROM.read(ptr + 1);
+
+        range++;
+    }
+
+    return range;
+}
+
+void _set_range(coord* values, uint8_t start, uint8_t size) {
+    for (uint8_t ptr = start; ptr <= ptr + (2 * size); ptr += 2) {
+        EEPROM.write(ptr, values->x);
+        EEPROM.write(ptr + 1, values->y);
+    }
+}
+
+uint8_t get_crate_width() {
+    return (uint8_t) EEPROM.read(CRATE_WIDTH_ADDR);
+}
+
+void set_crate_width(uint8_t value) {
+    EEPROM.write(CRATE_WIDTH_ADDR, value);
+}
+
+uint8_t get_crate_height() {
+    return (uint8_t) EEPROM.read(CRATE_HEIGHT_ADDR);
+}
+
+void set_crate_height(uint8_t value) {
+    EEPROM.write(CRATE_HEIGHT_ADDR, value);
+}
+
+uint8_t get_matrix_width() {
+    return (uint8_t) EEPROM.read(MATRIX_WIDTH_ADDR);
+}
+
+void set_matrix_width(uint8_t value) {
+    EEPROM.write(MATRIX_WIDTH_ADDR, value);
+}
+
+uint8_t get_matrix_height() {
+    return (uint8_t) EEPROM.read(MATRIX_HEIGHT_ADDR);
+}
+
+void set_matrix_height(uint8_t value) {
+    EEPROM.write(MATRIX_HEIGHT_ADDR, value);
+}
+
+coord* get_matrix_shape() {
+    uint8_t size = (uint8_t) EEPROM.read(MATRIX_SIZE_ADDR);
+    uint8_t start_addr = (uint8_t) EEPROM.read(MATRIX_START_ADDR);
+
+    return _get_range(start_addr, size);
+}
+
+coord* get_crate_shape() {
+    uint8_t size = (uint8_t) EEPROM.read(CRATE_SIZE_ADDR);
+    uint8_t start_addr = (uint8_t) DYN_MEMORY_ADDR;
+
+    return _get_range(start_addr, size);
+}
+
+void set_shape(coord* crate_shape,  size_t crate_size,
+               coord* matrix_shape, size_t matrix_size) {
+
+    EEPROM.write(CRATE_SIZE_ADDR, crate_size);
+    EEPROM.write(MATRIX_SIZE_ADDR, matrix_size);
+
+    uint8_t matrix_start_addr = DYN_MEMORY_ADDR + (2 * crate_size) + 1;
+    EEPROM.write(MATRIX_START_ADDR, matrix_start_addr);
+
+    _set_range(crate_shape, DYN_MEMORY_ADDR, crate_size);
+    _set_range(matrix_shape, matrix_start_addr, matrix_size);
+}
