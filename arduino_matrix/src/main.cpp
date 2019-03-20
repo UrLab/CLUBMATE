b2f733fce8b4c03b03c146de0b775c93392b0768
Thibault Vanwersch <thibault@vanwersch.fr>
2019-03-20 18:25:57 +0100

    let's remove some debug print

--- arduino_matrix/src/main.cpp
+++ arduino_matrix/src/main.cpp
@@ -8,18 +8,22 @@ COBSPacketSerial packetSerial;
 FastCRC16 CRC16;
 
 MateEEPROM mate_eeprom;
-CMMatrix cm(
+CMMatrix *cm;
+
+void setup() {
+    packetSerial.begin(115200);
+    packetSerial.setPacketHandler(&onPacketReceived);
+
+    mate_eeprom.reset_eeprom();
+    delay(100);
+
+    cm = new CMMatrix(
         mate_eeprom.get_crate_size(),
         mate_eeprom.get_matrix_size(),
         mate_eeprom.get_matrix_shape(),
         20, 4, 5,  //TODO put that in eeprom
         mate_eeprom.get_crate_shape()
-);
-
-void setup() {
-    mate_eeprom.reset_eeprom();
-    packetSerial.begin(115200);
-    packetSerial.setPacketHandler(&onPacketReceived);
+    );
 }
 
 void loop() {
@@ -45,23 +49,12 @@ uint8_t is_crc_correct(const uint8_t *data, size_t size) {
 }
 
 void onPacketReceived(const uint8_t* buffer, size_t size) {
-    Serial.print("Received : [");
-    for (int i = 0; i < (int)  size; i++) {
-        Serial.print(i);
-        Serial.print(" -> ");
-        Serial.print(buffer[i]);
-        Serial.print(", ");
-    }
-    Serial.println("]");
-
     if (size < 5) {
-        Serial.println("size < 5");
         uint8_t resp[5] = {0, 0, 0, 0, MSG_BAD_MSG};
         crc_and_send(resp, 5);
         return;
     }
     if (! is_crc_correct(buffer, size)) {
-        Serial.println("bad crc");
         uint8_t resp[5] = {0, 0, 0, 0, MSG_BAD_MSG};
         crc_and_send(resp, 5);
         return;
@@ -78,11 +71,9 @@ void onPacketReceived(const uint8_t* buffer, size_t size) {
             break;
         case UNI_COLOR:
             {
-                Serial.println("UNI_COLOR");
                 if (size == 9) {
-                    cm.fill(buffer[5], buffer[6], buffer[7], buffer[8]);
-                    draw(cm.render(), cm.get_leds_nbr());
-                    Serial.println("Done ...");
+                    cm->fill(buffer[5], buffer[6], buffer[7], buffer[8]);
+                    draw(cm->render(), cm->get_leds_nbr());
                     uint8_t resp[5] = {0, 0, buffer[2], buffer[3], OK};
                     crc_and_send(resp, 5);
                 } else {
@@ -93,16 +84,9 @@ void onPacketReceived(const uint8_t* buffer, size_t size) {
             break;
         case SET_PIXEL:
             {
-                Serial.println("SET_PIXEL");
-                Serial.print("x: ");
-                Serial.print(buffer[6]);
-                Serial.print(", y: ");
-                Serial.println(buffer[8]);
-
-                Serial.print(", y: ");
                 if (size == 11) {
-                    cm.mset(buffer[5], buffer[6], buffer[7], buffer[8], buffer[9], buffer[10]);
-                    draw(cm.render(), cm.get_leds_nbr());
+                    cm->mset(buffer[5], buffer[6], buffer[7], buffer[8], buffer[9], buffer[10]);
+                    draw(cm->render(), cm->get_leds_nbr());
                     uint8_t resp[5] = {0, 0, buffer[2], buffer[3], OK};
                     crc_and_send(resp, 5);
                 } else {
